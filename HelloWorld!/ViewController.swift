0d297bcabe6019118de8d81bd068d54e51c95f1b
Ilona Shcherbakova <shcherbakova0911@mail.ru>
2019-09-23 21:17:20 +0300

    Setup interface

--- HelloWorld!/ViewController.swift
+++ HelloWorld!/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showTextPressed() {
+    }
+    
 }
 
